2df265ab8533a6bf41ab7b29b5260e79ff7711af
VasifXammedov <vasifvkh@code.edu.az>
2021-02-01 18:34:01 +0400

    Final lahiyeye bashladiq

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function () {
+    
+
+    let nav = document.querySelector("nav")
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 70) {
+            nav.classList.add("nav-scroll");
+        }
+         else {
+            nav.classList.remove("nav-scroll");
+        }
+    });
+
+     
+    $("#NavOpen").click(function(e){
+        $(".navigation").addClass("show");
+        e.stopPropagation()
+    })
+    $("#NavClose").click(function(e){
+        $(".navigation").removeClass("show");
+        e.stopPropagation()
+    })
+    $('html').click(function(){
+        if($("#navigation").hasClass("show")){
+         $("#navigation").removeClass("show");
+        }
+    })
+    
+})
